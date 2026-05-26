c1c03b8e8c7177ef0892247bc8e90332b8be6339
Luis Lopez <llopezl55@miumg.edu.gt>
2026-05-25 22:38:11 -0600

    fix

--- src/main/java/hospital_system/HospitalSystemApplication.java
+++ src/main/java/hospital_system/HospitalSystemApplication.java
@@ -3,7 +3,7 @@ package hospital_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "hospital_system")
 public class HospitalSystemApplication {
 
 	public static void main(String[] args) {
