0a02a78fe0f24e41d4e233a36d05d4deb53765b3
Luis Lopez <llopezl55@miumg.edu.gt>
2026-05-25 22:51:03 -0600

    fix datasource

--- src/main/java/hospital_system/HospitalSystemApplication.java
+++ src/main/java/hospital_system/HospitalSystemApplication.java
@@ -2,12 +2,15 @@ package hospital_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "hospital_system")
+@EnableJpaRepositories(basePackages = "hospital_system.repository")
+@EntityScan(basePackages = "hospital_system.model")
 public class HospitalSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HospitalSystemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalSystemApplication.class, args);
+    }
 }
